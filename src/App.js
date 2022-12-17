309885c7534a4aa34c4ff491072437968064dc27
Mitchell Armstrong <mra0211@gmail.com>
2022-12-17 14:40:12 -0500

    Created the About component and created a placeholder image and placeholder text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import About from "./components/About";
 import Navigation from "./components/Nav";
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
+      <About></About>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="d-flex my-5 me-5">
+      <section className="col-4 d-flex justify-content-center">
+        <img
+          className="personal-image"
+          src="https://via.placeholder.com/250"
+          alt="placeholder"
+        ></img>
+      </section>
+      <section className="col-6">
+        <aside>
+          <h1 className="text-center about-title">Who Am I?</h1>
+          <p className="about-paragraph-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </aside>
+      </section>
+    </div>
+  );
+}
+
+export default About;
