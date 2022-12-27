1c2cf7e6969b39bca87cbf4e58bff14d30afc1e5
Mitchell Armstrong <mra0211@gmail.com>
2022-12-26 21:41:22 -0500

    Changed the build script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "react-responsive-modal/styles.css";
+// import "react-responsive-modal/styles.css";
 import "./App.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
